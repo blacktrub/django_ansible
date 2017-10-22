60c5f6de8727b255e1aefc0bd66477543a987567
blacktrub <lightlocdog@gmail.com>
2017-10-22 03:53:55 +0300

    added functional to deploy script for work only with exists environments

--- schemes/scripts/deploy.py
+++ schemes/scripts/deploy.py
@@ -1,15 +1,20 @@
 import os
 import argparse
 
-# todo: need get only existing environments
-environments = ('prod', 'stage', 'dev',)
+PATH = os.path.abspath(os.curdir)
+ENVIRONMENTS_DIR = os.path.join(PATH, 'environments')
+
+environments = [
+    name for name in os.listdir(ENVIRONMENTS_DIR)
+    if os.path.isdir(os.path.join(ENVIRONMENTS_DIR, name))
+]
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
     '--inventory', '-i',
     type=str,
-    choices=list(environments),
+    choices=environments,
     help='specify inventory host path',
 )
 
